9f3f4aef43e1fa0caea2302753f038bc8803737e
vgMonky <126716555+vgMonky@users.noreply.github.com>
2023-05-30 20:16:23 -0300

    Add files via upload

---
+++ gou_funct.js
@@ -0,0 +1,8 @@
+
+function onMenuClick() {
+    var navbar = document.getElementById("navigation-bar");
+    var responsive_class_name = "responsive";
+
+    navbar.classList.toggle(responsive_class_name);
+}
+
